d0fe2476e3b676a3c16a74224a3f03996c7611d0
kellerchch <kellerchch@gmail.com>
2018-03-08 17:04:49 -0700

    updating

---
+++ server/middlewares/filter.js
@@ -0,0 +1,11 @@
+var notAllowed = [
+    'crap', 'shoot', 'darn'];
+
+module.exports = function( req, res, next ) {
+    while ( notAllowed.find( word => req.body.text.includes(word) ) ) {
+        const badWord = notAllowed.find( word => req.body.text.includes(word) );
+        req.body.text = req.body.text.replace( badWord, '*'.repeat( badWord.length ) );
+}
+
+next();
+};
